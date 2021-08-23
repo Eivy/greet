94897920210e7c92dbbcc1c4ba7a2c47b1113490
Eivy <modern.times.rock.and.roll+git@gmail.com>
2021-08-23 22:27:33 +0900

    init

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace greet
+{
+    public static class Hello
+    {
+        public static void Greet()
+        {
+            System.Console.WriteLine("Hello");
+        }
+    }
+}
